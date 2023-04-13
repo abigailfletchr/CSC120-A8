d571c0bab1a1ecf8388d28986c93ae284b398eb5
Your Name <you@example.com>
2023-04-13 10:01:45 -0400

    java doc comments game.java

--- Game.java
+++ Game.java
@@ -1,25 +1,45 @@
-import java.util.ArrayList;
-
 public class Game implements Contract {
 
-    Player lonelyPlayer; // the one singular player in this game
+    /**
+     * lonely player is the player created from the Player class
+     * lastAction is the last action completed by the player or in the game
+     */
+    Player lonelyPlayer;
     String lastAction;
 
+    /**
+     * Default constructor for the Game class
+     */
     public Game(){
         this.lonelyPlayer = new Player();
         lastAction = null;
         System.out.println("Our game has been created.");
     }
 
+    /**
+     * Method that grabs an item and puts it in the players left hand
+     * @param item the object grabbed
+     */
     public void grab(String item){
         lonelyPlayer.putInLeftHand(item);
     }
 
+    /**
+     * Method that drops an item and removes it from the players left hand
+     * @param item the object dropped
+     * @return a message saying the item is dropped
+     */
     public String drop(String item){
         lonelyPlayer.takeOutOfLeftHand(item);
         return item + " dropped.";
     }
 
+
+    /**
+     * Method that examines an item once it is put in the players right hand. 
+     * Item is kept if player wants it or it is dropped.
+     * @param item the object examined
+     */
     public void examine(String item){
         if(this.lonelyPlayer.getWant()){
             this.lonelyPlayer.putInRightHand(item);
@@ -28,33 +48,58 @@ public class Game implements Contract {
         }
     } 
 
+    /**
+     * Method that "uses" or interacts with an object(item)
+     * @param item the object the player interacts with
+     */
     public void use(String item){
         System.out.println(this.lonelyPlayer.playWithItem(item));
     }
 
+    /**
+     * Method that makes the player walk
+     * @param direction the direction the player will walk in
+     */
     public boolean walk(String direction){
         return this.lonelyPlayer.walk(direction);
     }
 
+    /**
+     * Method that makes the player fly
+     * @param x the x coordinate
+     * @param y the y coordinate
+     */
     public boolean fly(int x, int y){
         return this.lonelyPlayer.fly(x, y);
     }
 
+    /**
+     * Abstract method that reduces the players size
+     */
     public Number shrink(){
         this.lonelyPlayer.changeSize(25);
         return this.lonelyPlayer.getSize();
     }
 
+    /**
+     * Abstract method that increases the players size
+     */
     public Number grow(){
         this.lonelyPlayer.changeSize(75);
         return this.lonelyPlayer.getSize();
     } 
 
+    /**
+     * Method that makes the player rest and adds health points if resting
+     */
     public void rest(){
         this.lonelyPlayer.addHealth(1);
         System.out.println("I'm laying down!");
     }
 
+    /**
+     * Method that "undo's" the previous action of the player or the game
+     */
     public void undo(){
         if(lastAction != null){
             if(lastAction == "grab"){
@@ -66,10 +111,8 @@ public class Game implements Contract {
             }else if(lastAction == "use"){
                 this.examine("Kettlebell");
             }else if(lastAction == "walk"){
-                // this.walk(oppositeDirection); // where am i getting these from
                 System.out.println("We're walking back.");
             }else if(lastAction == "fly"){
-                // this.fly(-x, -y); // where am i getting these from
                 System.out.println("We're flying back.");
             }else if(lastAction == "shrink"){
                 this.grow();
